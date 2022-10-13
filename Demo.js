acce664784d49be12d2d9e9b72bedf57fe61e4ec
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-10-13 14:11:30 +0200

    Added tests

---
+++ Demo.js
@@ -0,0 +1,20 @@
+import { Selector } from "testcafe"
+
+fixture`Week number test`
+    .page("https://weeknumber.com/");
+
+test("Link to Danish version", async t => {
+    await t
+        .expect(Selector("#ugenr").innerText).contains("Week")
+        .click("#menu > footer > ul > li:nth-child(1) > a")
+        .click("a[hreflang='da']")
+        .expect(Selector("#ugenr").innerText).contains("Uge")
+})
+
+test("Parse week and year", async t => {
+    await t
+        .typeText("#q", "12 2012")
+        .pressKey("enter")
+        .expect(Selector("#ugenr").innerText).eql("Week 12")
+        .expect(Selector("#description").innerText).contains("2012")
+})
---
+++ EASV.js
@@ -0,0 +1,16 @@
+import { Selector } from "testcafe"
+
+fixture`EASV programmes`
+    .page("https://www.easv.dk/en/programmes/");
+
+test("Filter test", async t => {
+    const interestSelect = Selector("#root > div > div.overview-wrapper > section > div > div.l-education-cards__row-center > div > div > div > div:nth-child(1) > select");
+    const interestOption = interestSelect.find("option");
+
+    await t
+        .click(interestSelect)
+        .click(interestOption.withText("IT & Media"))
+        .expect(Selector(".l-education-cards__card.o-card-tilt").count).eql(5)
+        .click("#root > div > div.overview-wrapper > section > div > div.l-education-cards__row-center > div > div > div > div:nth-child(2) > div > div > button:nth-child(2) > span")
+        .expect(Selector(".l-education-cards__card.o-card-tilt").count).eql(1)
+})

47e5c79cfd3c3e1a0210083a2e57f53bc81c6425
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-10-13 14:50:17 +0200

    Take screenshots

--- EASV.js
+++ EASV.js
@@ -10,6 +10,7 @@ test("Filter test", async t => {
     await t
         .click(interestSelect)
         .click(interestOption.withText("IT & Media"))
+        .takeScreenshot()
         .expect(Selector(".l-education-cards__card.o-card-tilt").count).eql(5)
         .click("#root > div > div.overview-wrapper > section > div > div.l-education-cards__row-center > div > div > div > div:nth-child(2) > div > div > button:nth-child(2) > span")
         .expect(Selector(".l-education-cards__card.o-card-tilt").count).eql(1)
